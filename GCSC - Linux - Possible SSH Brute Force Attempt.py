e2c23d641d53d92f54224432f9586e4b690fe0f7
VuThanhLong012 <vuthanhlong1542003@gmail.com>
2026-03-20 15:55:58 +0700

    Update GCSC - Linux - Possible SSH Brute Force Attempt.py

--- GCSC - Linux - Possible SSH Brute Force Attempt.py
+++ GCSC - Linux - Possible SSH Brute Force Attempt.py
@@ -1,16 +1,16 @@
 # Rule đã ổn => thay bằng macro linux_secure
 index=vpa_os_linux sourcetype=linux_secure signature="Failed password"  
-| lookup vpa_linux.csv hostname OUTPUT src_ip 
+| lookup vpa_linux.csv hostname OUTPUT src_ip
 | where isnotnull(src_ip)
 | bucket _time span=5m
 | stats 
     count as failed_attempts
-    min(_time) as first_time
-    max(_time) as last_time
-    values(hostname) as dest
-    values(src_port) as src_port
-    by _time tenant hostname src_ip user_name
+    earliest(_time) as first_time
+    latest(_time) as last_time
+    values(hostname) as dest_host
+    values(src_port) as src_ports
+    by _time tenant src_ip user_name
 | where failed_attempts >= 5
 | eval first_seen=strftime(first_time,"%Y-%m-%d %H:%M:%S")
 | eval last_seen=strftime(last_time,"%Y-%m-%d %H:%M:%S")
-| table _time tenant hostname src_ip user_name failed_attempts first_seen last_seen 
+| table _time tenant src_ip user_name failed_attempts first_seen last_seen dest_host src_ports
