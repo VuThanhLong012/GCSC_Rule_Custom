1e158dd8a4fd2b6291b37eaeab2850c791798cb8
VuThanhLong012 <vuthanhlong1542003@gmail.com>
2026-03-20 16:04:15 +0700

    Update GCSC - Linux - Possible Spraying Attempt.py

--- GCSC - Linux - Possible Password Spraying Attempt.py
+++ GCSC - Linux - Possible Password Spraying Attempt.py
@@ -1,7 +1,20 @@
-index=vpa_os_linux sourcetype=linux_secure # Rule đã ổn => thay bằng macro linux_secure
-| search signature IN ("Failed password", "authentication failure", "Invalid user")
-| lookup vpa_linux.csv hostname OUTPUT src_ip 
-| where isnotnull(src_ip)
+# Rule đã ổn => thay bằng macro linux_secure
+index=vpa_os_linux sourcetype=linux_secure
+| search signature IN ("Failed password", "Invalid user")
+| lookup vpa_linux.csv hostname OUTPUT src_ip
+| where isnotnull(src_ip) AND isnotnull(user_name)
 | bucket _time span=5m
-| stats count as failed_attempts values(signature) as signatures values(src_port) as src_port dc(user_name) as targeted_users values(user_name) as user_name by _time tenant src_ip
+| stats 
+    count as failed_attempts
+    dc(user_name) as targeted_users
+    values(user_name) as targeted_account_list
+    values(signature) as signatures
+    earliest(_time) as first_time
+    latest(_time) as last_time
+    values(hostname) as src_host
+    values(src_port) as src_ports
+    by _time tenant src_ip
 | where failed_attempts >= 5 AND targeted_users >= 3
+| eval first_seen=strftime(first_time,"%Y-%m-%d %H:%M:%S")
+| eval last_seen=strftime(last_time,"%Y-%m-%d %H:%M:%S")
+| table _time tenant src_ip src_host src_ports failed_attempts targeted_users targeted_account_list signatures first_seen last_seen
