23c32c12637770afa72bc0813efb92a32ce66ed3
VuThanhLong012 <vuthanhlong1542003@gmail.com>
2026-03-27 15:49:46 +0700

    Update GCSC - Linux - Possible Spraying Attempt.py

--- GCSC - Linux - Possible Spraying Attempt.py
+++ GCSC - Linux - Possible Spraying Attempt.py
@@ -3,6 +3,7 @@ index=vpa_os_linux sourcetype=linux_secure
 | search signature IN ("Failed password", "Invalid user")
 | lookup vpa_linux.csv hostname OUTPUT src_ip
 | where isnotnull(src_ip) AND isnotnull(user_name)
+| rename hostname as dest
 | bucket _time span=5m
 | stats 
     count as failed_attempts
@@ -11,10 +12,10 @@ index=vpa_os_linux sourcetype=linux_secure
     values(signature) as signatures
     earliest(_time) as first_time
     latest(_time) as last_time
-    values(hostname) as src_host
+    values(dest) as dest_host
     values(src_port) as src_ports
     by _time tenant src_ip
 | where failed_attempts >= 5 AND targeted_users >= 3
 | eval first_seen=strftime(first_time,"%Y-%m-%d %H:%M:%S")
 | eval last_seen=strftime(last_time,"%Y-%m-%d %H:%M:%S")
-| table _time tenant src_ip src_host src_ports failed_attempts targeted_users targeted_account_list signatures first_seen last_seen
+| table _time tenant src_ip dest_host src_ports failed_attempts targeted_users targeted_account_list signatures first_seen last_seen
