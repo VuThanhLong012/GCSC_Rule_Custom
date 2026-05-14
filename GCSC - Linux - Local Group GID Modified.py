5be9ebcec2d85c97c81a07a777ba10c84ac7994d
VuThanhLong012 <vuthanhlong1542003@gmail.com>
2026-05-14 10:40:00 +0700

    Update GCSC - Linux - Local Group GID Modified.py

--- GCSC - Linux - Local Group GID Modified.py
+++ GCSC - Linux - Local Group GID Modified.py
@@ -1,19 +1,22 @@
-# Rule đã ổn => thay bằng macro linux_secure
-index=vpa_os_linux sourcetype=linux_secure "groupmod[" "new gid:"
-| lookup vpa_linux.csv hostname OUTPUT src_ip
-| where isnotnull(src_ip)
-| rename hostname as dest
-| rex field=_raw "groupmod\[\d+\]:\s+group changed in /etc/group \(group (?<group>[A-Za-z0-9._-]+)/(?<old_gid>\d+), new gid: (?<new_gid>\d+)\)"
-| where isnotnull(group) AND isnotnull(old_gid) AND isnotnull(new_gid)
-| eval app="groupmod"
-| eval action="modify"
-| eval signature="local_group_gid_modified"
-| eval first_seen=strftime(_time,"%Y-%m-%d %H:%M:%S")
-| table _time tenant src_ip dest group old_gid new_gid app action signature first_seen
+#Trạng thái: Đã test
+#NOTE: 
+- Cần bổ sung 1 số filed đặc trưng như tenant 
+- Thêm lookup tương ứng
+- Ở đây em parse bằng app Unix and Linux có sẵn nên 1 số field app không parse được em sẽ dùng rex (Chỗ rex này sẽ nhờ anhtq theo đó mà parse ra field)
 
-# thay đổi thành viên group sẵn có
+index=linux sourcetype=linux_secure "groupmod" "new gid:" "/etc/group"
+| rename host as dest
+| rex field=_raw "group\s+\(group\s+(?<group>[^/]+)/(?<old_gid>\d+),\s+new\s+gid:\s+(?<new_gid>\d+)"
+| eval first_seen=strftime(_time,"%Y-%m-%d %H:%M:%S")
+| table _time pid dest group old_gid new_gid process first_seen
+
 #SAMPLE
 '''
-Aug 30 20:39:03 aladdin groupmod[2450]: group changed in /etc/group (group test/501, new gid: 502)
-Aug 30 20:39:03 aladdin groupmod[2450]: group changed in /etc/passwd (group test/501, new gid: 502)
+5/14/26
+10:18:55.932 AM	
+2026-05-14T10:18:55.932300+07:00 AGENT groupmod[32862]: group changed in /etc/passwd (group testgroup/1003, new gid: 2000)
+host = AGENTsource = /var/log/auth.logsourcetype = linux_secure
+5/14/26
+10:18:55.932 AM	
+2026-05-14T10:18:55.932003+07:00 AGENT groupmod[32862]: group changed in /etc/group (group testgroup/1003, new gid: 2000)
 '''
