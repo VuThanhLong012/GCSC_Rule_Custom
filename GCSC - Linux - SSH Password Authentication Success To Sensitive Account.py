dfaec5b821348c3ea9c4ed5b39cdcb49972e180d
VuThanhLong012 <vuthanhlong1542003@gmail.com>
2026-05-14 12:16:48 +0700

    Update GCSC - Linux - SSH Password Authentication Success To Sensitive Account.py

--- GCSC - Linux - SSH Password Authentication Success To Sensitive Account.py
+++ GCSC - Linux - SSH Password Authentication Success To Sensitive Account.py
@@ -1,20 +1,52 @@
-# Rule đã ổn => thay bằng macro linux_secure
-index=vpa_os_linux sourcetype=linux_secure signature="Accepted password"
-| lookup vpa_linux.csv hostname OUTPUT src_ip
-| where isnotnull(src_ip)
-| rename hostname as dest, user_name as user
-| where match(user,"^(root|admin|oracle|postgres|mysql|mongodb|redis|nginx|apache|tomcat|jenkins|splunk|zabbix|backup|deploy|ansible|svc_.*)$")
-| rex field=_raw "Accepted\s+password\s+for\s+(?<user>\S+)\s+from\s+(?<src>\d{1,3}(?:\.\d{1,3}){3})\s+port\s+(?<src_port>\d+)"
-| where isnotnull(src)
-| eval auth_method="password"
-| eval app="sshd"
-| eval action="success"
-| eval signature="ssh_password_auth_success_to_sensitive_account"
-| eval first_seen=strftime(_time,"%Y-%m-%d %H:%M:%S")
-| table _time tenant src_ip dest src src_port user auth_method app action signature first_seen
+#Trạng thái: Đã test
+#NOTE: 
+- Cần bổ sung 1 số filed đặc trưng như tenant 
+- Thêm lookup tương ứng để lấy src
+- Ở đây em parse bằng app Unix and Linux có sẵn nên 1 số field app không parse được em sẽ dùng rex (Chỗ rex này sẽ nhờ anhtq theo đó mà parse ra field)
+
+index=linux sourcetype=linux_secure 
+| search signature="Accepted password" process="sshd"
+| rename host as dest, user_name as user
+| search user IN ("root", "admin", "oracle", "postgres", "mysql", "mongodb", "redis", "nginx", "apache", "tomcat", "jenkins", "splunk", "zabbix", "backup", "deploy", "ansible") OR user="svc_*"
+| eval first_seen=strftime(_time, "%Y-%m-%d %H:%M:%S")
+| table src_ip dest src_port user process action signature first_seen
 
 # Bắt khi một account nhạy cảm đăng nhập SSH thành công bằng password thay vì key
 #SAMPLE
 '''
-May 21 20:22:28 slacker2 sshd[8813]: Accepted password for root from 192.168.20.185 port 1066 ssh2
+5/14/26
+12:03:50.892 PM	
+2026-05-14T12:03:50.892848+07:00 AGENT systemd-logind[1095]: Removed session 26.
+host = AGENTsource = /var/log/auth.logsourcetype = linux_secure
+5/14/26
+12:03:50.882 PM	
+2026-05-14T12:03:50.882630+07:00 AGENT systemd-logind[1095]: Session 26 logged out. Waiting for processes to exit.
+host = AGENTsource = /var/log/auth.logsourcetype = linux_secure
+5/14/26
+12:03:50.869 PM	
+2026-05-14T12:03:50.869778+07:00 AGENT sshd[34230]: pam_unix(sshd:session): session closed for user root
+host = AGENTsource = /var/log/auth.logsourcetype = linux_secure
+5/14/26
+12:03:50.867 PM	
+2026-05-14T12:03:50.867384+07:00 AGENT sshd[34230]: Disconnected from user root 127.0.0.1 port 46110
+host = AGENTsource = /var/log/auth.logsourcetype = linux_secure
+5/14/26
+12:03:50.867 PM	
+2026-05-14T12:03:50.867077+07:00 AGENT sshd[34230]: Received disconnect from 127.0.0.1 port 46110:11: disconnected by user
+host = AGENTsource = /var/log/auth.logsourcetype = linux_secure
+5/14/26
+12:03:47.892 PM	
+2026-05-14T12:03:47.892563+07:00 AGENT systemd-logind[1095]: New session 26 of user root.
+host = AGENTsource = /var/log/auth.logsourcetype = linux_secure
+5/14/26
+12:03:47.876 PM	
+2026-05-14T12:03:47.876080+07:00 AGENT sshd[34230]: pam_unix(sshd:session): session opened for user root(uid=0) by root(uid=0)
+host = AGENTsource = /var/log/auth.logsourcetype = linux_secure
+5/14/26
+12:03:47.872 PM	
+2026-05-14T12:03:47.872489+07:00 AGENT sshd[34230]: Accepted password for root from 127.0.0.1 port 46110 ssh2
+host = AGENTsource = /var/log/auth.logsourcetype = linux_secure
+5/14/26
+12:03:40.676 PM	
+2026-05-14T12:03:40.676119+07:00 AGENT gdm-password]: gkr-pam: unlocked login keyring
 '''
